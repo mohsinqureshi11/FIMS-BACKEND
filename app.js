98b943767248e0354b2cc2654517e09f06b58e42
mohsin_development <mq0062783@gmail.com>
2025-08-15 00:00:11 +0530

    changes for db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json({limit:"200mb"}))
 app.use(express.urlencoded({limit:"200mb",extended:true}))
 
 const corsOptions ={
-    origin : "https://fieldinspectionmanagmentsystem.netlify.app/",
+    origin : "https://fieldinspectionmanagmentsystem.netlify.app",
     Credential:true,
     optionSuccessStatus:200 
 }
