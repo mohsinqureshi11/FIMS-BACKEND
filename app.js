2806c7ba562f0eee5032994c8bae542958f71de0
mohsin_development <mq0062783@gmail.com>
2025-08-15 16:37:27 +0530

    Integrated officer list in FormBuilder

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ import officerRoute from "./src/api/officer/route.js"
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,  // dynamic frontend URL from .env
-  methods:["POST","GET"],
-  // credentials: true,                  // JB authontication ka use ho tb
-  optionsSuccessStatus: 200           // spelling fix
-}));
+app.use(cors());
 
 app.use(express.json({limit:"200mb"}))   
 app.use(express.urlencoded({limit:"200mb",extended:true}))
